a49012be9eecbf70b9e09f01ccfa6983460f1dcb
stclair <wes.stclair@gmail.com>
2014-09-09 21:36:39 -0500

    bleh

--- js/components.js
+++ js/components.js
@@ -1,21 +1,12 @@
-var thisSucks = new function() {
-    this.assailant = null;
-    this.drop = function(target) {
-        console.log(this.assailant.get('project.id'));
-        console.log(target.get('project.id'));
-    }
-}
-
 ProjectPriority.DraggableProjectComponent = Ember.Component.extend({
     dragStart: function(event) {
       event.dataTransfer.setData('text/data', this.get('project.id'));
-      thisSucks.assailant = this;
     },
     dragOver: function(event) {
         event.preventDefault();
     },
     drop: function(event) {
         var id = event.dataTransfer.getData('text/data');
-        thisSucks.drop(this);
+        debugger
     }
 });
